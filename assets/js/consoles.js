9761600f4de5a124fc38e574925ad0975baf9c3f
JustinTien <thitbbeb+git-commit@gmail.com>
2023-06-04 12:28:24 +0800

    fix: :beetle:

---
+++ assets/js/consoles.js
@@ -0,0 +1,411 @@
+var config = {
+  companies: [
+    {
+      "company": "3DO"
+    },
+    {
+      "company": "APF Electronics"
+    },
+    {
+      "company": "Atari"
+    },
+    {
+      "company": "Coleco"
+    },
+    {
+      "company": "MAT"
+    },
+    {
+      "company": "Magnavox"
+    },
+    {
+      "company": "Microsoft"
+    },
+    {
+      "company": "NEC"
+    },
+    {
+      "company": "Nintendo"
+    },
+    {
+      "company": "SEGA"
+    },
+    {
+      "company": "SNK"
+    },
+    {
+      "company": "Sony"
+    }
+  ],
+  generations: [
+    {
+      generation: 1,
+      period: '1972－1977',
+    },
+    {
+      generation: 2,
+      period: '1972－1977',
+    },
+    {
+      generation: 3,
+      period: '1983－1987',
+    },
+    {
+      generation: 4,
+      period: '1987－1994',
+    },
+    {
+      generation: 5,
+      period: '1993－2002',
+    },
+    {
+      generation: 6,
+      period: '1998－2006',
+    },
+    {
+      generation: 7,
+      period: '2005－2012',
+    },
+    {
+      generation: 8,
+      period: '2012－',
+    },
+    {
+      generation: 9,
+      period: '2020－',
+    },
+  ],
+  consoles: [
+    {
+      generation: 1,
+      company: 'Magnavox',
+      name: 'Magnavox Odyssey (美格福斯奧德賽)',
+      release_date: '19720901',
+      cover: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Magnavox_Odyssey.jpg/300px-Magnavox_Odyssey.jpg',
+      wiki: 'https://zh.wikipedia.org/zh-tw/%E7%B1%B3%E7%BD%97%E5%8D%8E%E5%A5%A5%E5%BE%B7%E8%B5%9B'
+    },
+    {
+      generation: 1,
+      company: 'Atari',
+      name: 'Pong (乓)',
+      release_date: '19721129',
+      cover: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0e/PongVideoGameCabinet.jpg/300px-PongVideoGameCabinet.jpg',
+      wiki: 'https://zh.wikipedia.org/zh-tw/%E4%B9%93',
+    },
+    {
+      generation: 1,
+      company: 'Coleco',
+      name: 'Coleco Telstar',
+      release_date: '19760101',
+      cover: 'https://upload.wikimedia.org/wikipedia/commons/e/ef/Ctelstar.jpg',
+      wiki: 'https://zh.wikipedia.org/zh-tw/Telstar_(%E9%81%8A%E6%88%B2%E6%A9%9F)',
+    },
+    {
+      generation: 1,
+      company: 'APF Electronics',
+      name: 'APF TV Fun',
+      release_date: '19760101',
+      cover: 'https://upload.wikimedia.org/wikipedia/commons/8/87/APF_TV_Fun_%28model_442%29_-_1977.jpg',
+      wiki: 'https://zh.wikipedia.org/wiki/APF_TV_Fun',
+    },
+    {
+      generation: 1,
+      company: 'Nintendo',
+      name: 'Color TV-Game',
+      release_date: '19770601',
+      cover: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Nintendo-Color-TV-Game-Blockbreaker-FL.jpg/300px-Nintendo-Color-TV-Game-Blockbreaker-FL.jpg',
+      wiki: 'https://zh.wikipedia.org/zh-tw/Color_TV-Game',
+    },
+    {
+      generation: 2,
+      company: 'Atari',
+      name: 'Atari VCS (1982年11月以後改名為 Atari 2600)',
+      release_date: '19770911',
+      cover: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/dc/Atari2600a.JPG/300px-Atari2600a.JPG',
+      wiki: 'https://zh.wikipedia.org/zh-tw/%E9%9B%85%E9%81%94%E5%88%A92600',
+    },
+    {
+      generation: 2,
+      company: 'Magnavox',
+      name: 'Magnavox Odyssey² (美格福斯奧德賽²)',
+      release_date: '19780701',
+      cover: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/Videopac-pete-screen-800px.jpg/300px-Videopac-pete-screen-800px.jpg',
+      wiki: 'https://zh.wikipedia.org/zh-tw/%E7%B1%B3%E7%BD%97%E5%8D%8E%E5%A5%A5%E5%BE%B7%E8%B5%9B%C2%B2',
+    },
+    {
+      generation: 2,
+      company: 'MAT',
+      name: 'Intellivision',
+      release_date: '19790101',
+      cover: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/Intellivision_-_gi_1326971.jpg/300px-Intellivision_-_gi_1326971.jpg',
+      wiki: 'https://zh.wikipedia.org/zh-tw/Intellivision',
+    },
+    {
+      generation: 2,
+      company: 'Coleco',
+      name: 'ColecoVision',
+      release_date: '19820801',
+      cover: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4b/ColecoVision-wController-L.jpg/300px-ColecoVision-wController-L.jpg',
+      wiki: 'https://zh.wikipedia.org/zh-tw/ColecoVision',
+    },
+    {
+      generation: 3,
+      company: 'Nintendo',
+      name: 'FC/NES (Family Computer)',
+      release_date: '19830715',
+      cover: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/06/Nintendo-Famicom-Console-Set-FL.jpg/300px-Nintendo-Famicom-Console-Set-FL.jpg',
+      wiki: 'https://zh.wikipedia.org/zh-tw/%E7%BA%A2%E7%99%BD%E6%9C%BA',
+    },
+    {
+      generation: 3,
+      company: 'SEGA',
+      name: 'SG-1000 (阿羅士)',
+      release_date: '19830715',
+      cover: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7b/Sega-SG-1000-Console-Set.jpg/300px-Sega-SG-1000-Console-Set.jpg',
+      wiki: 'https://zh.wikipedia.org/zh-tw/SG-1000',
+    },
+    {
+      generation: 3,
+      company: 'SEGA',
+      name: 'SMS/世嘉四代 (SEGA Master System)',
+      release_date: '19851020',
+      cover: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Sega-Master-System-Set.jpg/300px-Sega-Master-System-Set.jpg',
+      wiki: 'https://zh.wikipedia.org/zh-tw/%E4%B8%96%E5%98%89Master_System',
+    },
+    {
+      generation: 3,
+      company: 'Atari',
+      name: 'Atari 7800 (Atari 7800 ProSystem)',
+      release_date: '19860501',
+      cover: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Atari-7800-Console-Set.jpg/300px-Atari-7800-Console-Set.jpg',
+      wiki: 'https://zh.wikipedia.org/zh-tw/%E9%9B%85%E8%BE%BE%E5%88%A97800',
+    },
+    {
+      generation: 4,
+      company: 'NEC',
+      name: 'PCE/TurboGrafx-16 (PC Engine)',
+      release_date: '19871030',
+      cover: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/PC-Engine_FirstModel.jpg/180px-PC-Engine_FirstModel.jpg',
+      wiki: 'https://zh.wikipedia.org/zh-tw/PC_Engine',
+    },
+    {
+      generation: 4,
+      company: 'SEGA',
+      name: 'MD/世嘉五代(Mega Drive)',
+      release_date: '19881029',
+      cover: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/SegaMegadrive.jpg/360px-SegaMegadrive.jpg',
+      wiki: 'https://zh.wikipedia.org/zh-tw/Mega_Drive',
+    },
+    {
+      generation: 4,
+      company: 'SNK',
+      name: 'Neo Geo AES (Neo Geo Advanced Entertainment System)',
+      release_date: '19900426',
+      cover: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/Neogeoaes.jpg/360px-Neogeoaes.jpg',
+      wiki: 'https://zh.wikipedia.org/zh-tw/Neo_Geo',
+    },
+    {
+      generation: 4,
+      company: 'Nintendo',
+      name: 'SFC/SNES/超級任天堂 (Super Famicom)',
+      release_date: '19901121',
+      cover: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ee/Nintendo-Super-Famicom-Set-FL.jpg/420px-Nintendo-Super-Famicom-Set-FL.jpg',
+      wiki: 'https://zh.wikipedia.org/zh-tw/%E8%B6%85%E7%B4%9A%E4%BB%BB%E5%A4%A9%E5%A0%82',
+    },
+    {
+      generation: 5,
+      company: '3DO',
+      name: '3DO (3DO Interactive Multiplayer)',
+      release_date: '19931004',
+      cover: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a8/3D0_game_console.jpg/300px-3D0_game_console.jpg',
+      wiki: 'https://zh.wikipedia.org/zh-tw/3DO',
+    },
+    {
+      generation: 5,
+      company: 'SEGA',
+      name: 'SS/SAT/土星 (SEGA Saturn)',
+      release_date: '19941122',
+      cover: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/Saturnconsole.jpg/300px-Saturnconsole.jpg',
+      wiki: 'https://zh.wikipedia.org/zh-tw/%E4%B8%96%E5%98%89%E5%9C%9F%E6%98%9F',
+    },
+    {
+      generation: 5,
+      company: 'Sony',
+      name: 'PS/PS1/PSX (PlayStation)',
+      release_date: '19941203',
+      cover: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/PSX-Console-wController.png/420px-PSX-Console-wController.png',
+      wiki: 'https://zh.wikipedia.org/zh-tw/PlayStation_(%E9%81%8A%E6%88%B2%E6%A9%9F),'
+    },
+    {
+      generation: 5,
+      company: 'Nintendo',
+      name: 'N64/任天堂64 (Nintendo 64)',
+      release_date: '19960623',
+      cover: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Nintendo_64.jpg/300px-Nintendo_64.jpg',
+      wiki: 'https://zh.wikipedia.org/zh-tw/%E4%BB%BB%E5%A4%A9%E5%A0%8264',
+    },
+    {
+      generation: 6,
+      company: 'SEGA',
+      name: 'DC (Dreamcast)',
+      release_date: '19981127',
+      cover: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/DreamcastConsole.png/300px-DreamcastConsole.png',
+      wiki: 'https://zh.wikipedia.org/zh-tw/Dreamcast',
+    },
+    {
+      generation: 6,
+      company: 'Sony',
+      name: 'PS2 (PlayStation 2)',
+      release_date: '20000304',
+      cover: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/PlayStation_2.png/220px-PlayStation_2.png',
+      wiki: 'https://zh.wikipedia.org/zh-tw/PlayStation_2',
+    },
+    {
+      generation: 6,
+      company: 'Nintendo',
+      name: 'NGC/GCN (Nintendo GameCube)',
+      release_date: '20010914',
+      cover: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/NGC_Gamecube.jpg/300px-NGC_Gamecube.jpg',
+      wiki: 'https://zh.wikipedia.org/zh-tw/%E4%BB%BB%E5%A4%A9%E5%A0%82GameCube',
+    },
+    {
+      generation: 6,
+      company: 'Microsoft',
+      name: 'Xbox',
+      release_date: '20011115',
+      cover: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Xbox_1.jpg/300px-Xbox_1.jpg',
+      wiki: 'https://zh.wikipedia.org/zh-tw/Xbox_(%E9%81%8A%E6%88%B2%E6%A9%9F)',
+    },
+    {
+      generation: 7,
+      company: 'Microsoft',
+      name: 'Xbox 360',
+      release_date: '20051122',
+      cover: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Xbox360.png/260px-Xbox360.png',
+      wiki: 'https://zh.wikipedia.org/zh-tw/Xbox_360',
+    },
+    {
+      generation: 7,
+      company: 'Sony',
+      name: 'PS3 (PlayStation 3)',
+      release_date: '20061111',
+      cover: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/Playstation3vector.svg/260px-Playstation3vector.svg.png',
+      wiki: 'https://zh.wikipedia.org/zh-tw/PlayStation_3',
+    },
+    {
+      generation: 7,
+      company: 'Nintendo',
+      name: 'Wii',
+      release_date: '20061119',
+      cover: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/Wii_Wiimotea.png/260px-Wii_Wiimotea.png',
+      wiki: 'https://zh.wikipedia.org/zh-tw/Wii',
+    },
+    {
+      generation: 8,
+      company: 'Nintendo',
+      name: 'Wii U',
+      release_date: '20121118',
+      cover: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4a/Wii_U_Console_and_Gamepad.png/600px-Wii_U_Console_and_Gamepad.png',
+      wiki: 'https://zh.wikipedia.org/zh-tw/Wii_U',
+    },
+    {
+      generation: 8,
+      company: 'Sony',
+      name: 'PS4 (PlayStation 4)',
+      release_date: '20131115',
+      cover: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/PlayStation_4_vertical.jpg/500px-PlayStation_4_vertical.jpg',
+      wiki: 'https://zh.wikipedia.org/zh-tw/PlayStation_4',
+    },
+    {
+      generation: 8,
+      company: 'Microsoft',
+      name: 'Xbox 360',
+      release_date: '20131122',
+      cover: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f7/New_XBox_360_and_XBox_One._%289021844483%29_crop2.jpg/500px-New_XBox_360_and_XBox_One._%289021844483%29_crop2.jpg',
+      wiki: 'https://zh.wikipedia.org/zh-tw/Xbox_One',
+    },
+    {
+      generation: 9,
+      company: 'Nintendo',
+      name: 'NS/Switch/任天堂Switch',
+      release_date: '20170303',
+      cover: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Nintendo-Switch-wJoyCons-BlRd-Standing-FL.jpg/460px-Nintendo-Switch-wJoyCons-BlRd-Standing-FL.jpg',
+      wiki: 'https://zh.wikipedia.org/zh-tw/%E4%BB%BB%E5%A4%A9%E5%A0%82Switch',
+    },
+    {
+      generation: 9,
+      company: 'Sony',
+      name: 'PS4 Pro (PlayStation 4 pro)',
+      release_date: '20161110',
+      cover: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/00/Sony-PlayStation4-Pro-Console-FR.jpg/720px-Sony-PlayStation4-Pro-Console-FR.jpg',
+      wiki: 'https://zh.wikipedia.org/zh-tw/PlayStation_4#PlayStation_4_Pro',
+    },
+    {
+      generation: 9,
+      company: 'Microsoft',
+      name: 'Xbox One X',
+      release_date: '20171107',
+      cover: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7b/XBOX_ONE_X_Gamescom_%2836042607743%29.jpg/480px-XBOX_ONE_X_Gamescom_%2836042607743%29.jpg',
+      wiki: 'https://zh.wikipedia.org/zh-tw/Xbox_One#Xbox_One_X',
+    },
+  ],
+}
+
+function generateTR (list = []) {
+  if (list.length === 0) return
+
+  let tr = ''
+  for (let i = 0; i < list.length; i++) {
+    const obj = list[i]
+    if (i === 0) {
+      const date = obj.release_date.match(/(\d{4})(\d{2})(\d{2})/).slice(1).join('/')
+      tr = `<tr>
+        <th class="actived" rowspan="${list.length}">${date}</th>
+        <td>
+          <a href="${obj.wiki}"><div>${obj.name}</div></a>
+        </td> 
+      </tr>`
+      continue
+    }
+    tr += `<tr>
+            <td>
+              <a href="${obj.wiki}"><div>${obj.name}</div></a>
+            </td>
+            </tr>`
+  }
+  
+  return tr
+}
+
+
+for(let index = 1; index <= 9; index++) {
+  var table = document.createElement('table')
+  var caption = document.createElement('caption')
+  var tbody = document.createElement('tbody')
+  let generationObj = config.generations.find(obj => obj.generation === index)
+  let list = config.consoles.filter(obj => obj.generation === index)
+
+  caption.innerText = `第${index}世代發行時間軸 (${generationObj.period})`
+  list.sort((a, b) => parseInt(a.release_date) - parseInt(b.release_date))
+  list
+    .reduce((acc, curr) => {
+      let find = acc.find(obj => obj.release_date === curr.release_date)
+      if (!find) {
+        find = {
+          release_date: curr.release_date,
+          list: []
+        }
+        acc.push(find)
+      }
+      find.list.push(curr)
+      return acc
+    }, [])
+    .forEach(obj => {
+      tbody.innerHTML += generateTR(obj.list)
+    })
+  table.classList.add('release-timeline')
+  table.classList.add('wikitable')
+  table.appendChild(caption)
+  table.appendChild(tbody)
+  document.body.appendChild(table)
+}
